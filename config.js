72d3dd84ea0c65eeda98fb244928cb7d9832045a
Haris Ratno Pambudi <harisratnopambudi@gmail.com>
2025-12-29 20:15:51 +0700

    Disable Netflix feature in config and add Shopee product listing

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-const BUYER_CONFIG = {
+var BUYER_CONFIG = {
     // License Key
-    licenseKey: "DEMO-VALID-KEY",
+    licenseKey: "D255D89573B3873CA169C697C446",
 
     // Business Name
     businessName: "AshrafHotspot",
 
     // Login Mode: 1 = Username/Voucher only, 2 = Username + Password
-    loginMode: 1
+    loginMode: 1,
+
+    // Enable Netflix Feature: 1 = Enable, 0 = Disable
+    enableNetflix: 0
 };
